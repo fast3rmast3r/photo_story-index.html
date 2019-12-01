6237d997f36d7ccdc695da171133a166d18441f4
Nikolay Ivanov <58340082+fast3rmast3r@users.noreply.github.com>
2019-12-01 09:11:49 +0200

    Complete Photo Story
    This is a project, for the Front-End course I am in. There's an easter egg for my colleagues from the course. The photographer is a really close friend of mine, and I really wanna thank her for letting me use her work for this project.

---
+++ javaScript/secret.js
@@ -0,0 +1,14 @@
+setInterval(
+  function randomColor() {
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    console.log(randomColor);
+  }, 1);
+
+setInterval(
+  function randomImage(){
+    var randomImg = Math.floor(Math.random() * 5) + 1;
+    var image = document.getElementById("image_"+randomImg);
+    image.style.visibility = "visible";
+    console.log(randomImg);
+  }, 40);
