126e83ec702a1417b23ca649d93299fb20310ae8
Nikolay Ivanov <58340082+fast3rmast3r@users.noreply.github.com>
2019-12-02 17:09:28 +0200

    Done for now.
    First validated by w3's validator, then beautified by codebeautify.

--- javaScript/are_you_sure.js
+++ javaScript/are_you_sure.js
@@ -1,8 +1,8 @@
 var click_on_me = document.getElementById('click_on_me');
 
 click_on_me.onclick = function() {
-  var x = Math.floor(Math.random() * 400);
-  var y = Math.floor(Math.random() * 400);
-  click_on_me.style.top = x + 'px';
-  click_on_me.style.left = y + 'px';
+ var x = Math.floor(Math.random() * 400);
+ var y = Math.floor(Math.random() * 400);
+ click_on_me.style.top = x + 'px';
+ click_on_me.style.left = y + 'px';
 };
