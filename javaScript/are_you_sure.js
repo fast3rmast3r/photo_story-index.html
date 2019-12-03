accaaddd379c9d7cedf762f21ae1cbc4b2a61961
Nikolay Ivanov <58340082+fast3rmast3r@users.noreply.github.com>
2019-12-03 17:27:04 +0200

    Update 1.1
    I am sorry for what I have done.

--- javaScript/are_you_sure.js
+++ javaScript/are_you_sure.js
@@ -1,6 +1,6 @@
 var click_on_me = document.getElementById('click_on_me');
 
-click_on_me.onclick = function() {
+click_on_me.onmouseover = function() {
  var x = Math.floor(Math.random() * 400);
  var y = Math.floor(Math.random() * 400);
  click_on_me.style.top = x + 'px';
